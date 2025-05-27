43eb927f7cad0c922e1e888ea25e7fcfd620c226
Juanextevam19 <estebanpalacio1019@gmail.com>
2025-05-27 11:02:29 -0500

    refactor se crea repositorio para proyecto uno

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String nombre="Jacobo Soto Rojas";
-        int edad=19;
+        String nombre1="Jacobo Soto Rojas";
+        String nombre2="Tatiana Poddubnyak";
+        String nombre3="Alejandra Carmona";
+        String nombre4="Sebastian Chávez";
+        String nombre5="Ana Sofia Muñoz Sosa";
+        String nombre6="Sebastian Garzon Villada";
+
+        int edad1=19;
+        int edad2=35;
+        int edad3=18;
+        int edad4=21;
+        int edad5=18;
+        int edad6=23;
+
+        int documento1=1017926485;
+        int documento2=453590;
+        int documento3=1033489179;
+        int documento4=1000190561;
+        int documento5=1018236586;
+        int documento6=1000654102;
     }
 }
